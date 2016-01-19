8c400f38ce15caf75c25fe9058baa18d1d7ce33d
TomoyaShibata <wind.of.hometown@gmail.com>
2016-01-19 16:04:34 +0900

    [add]プロジェクトファイル群追加

---
+++ Geopelia/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Prism.Windows.Mvvm;
+
+namespace Geopelia.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public int PivotItemWidth { get; set; } = 400;
+
+        private string _nowDateTime = DateTime.Now.ToString();
+        public string NowDateTime
+        {
+            get { return this._nowDateTime; }
+            set { this.SetProperty(ref this._nowDateTime, value); }
+        }
+
+        public MainPageViewModel()
+        {
+        }
+    }
+}
---
+++ Geopelia/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Geopelia.ViewModels;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace Geopelia.Views
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainPageViewModel ViewModel => this.DataContext as MainPageViewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 500);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+    }
+}
