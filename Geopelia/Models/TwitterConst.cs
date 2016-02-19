f8c004eff3365267fe240d1c6e6f40f2d969fecf
TomoyaShibata <wind.of.hometown@gmail.com>
2016-02-19 11:07:20 +0900

    [add] CI でコンパイルが撮らない問題を解消するため、依存ファイルを追加

---
+++ Geopelia/Models/TwitterConst.cs
@@ -0,0 +1,14 @@
+﻿namespace Geopelia.Models
+{
+    /// <summary>
+    /// コンシューマーキー群の値を格納するクラス
+    /// （認証機能が出来次第削除する）
+    /// </summary>
+    public class TwitterConst
+    {
+        public static string ConsumerKey       = "";
+        public static string ConsumerSecret    = "";
+        public static string AccessToken       = "";
+        public static string AccessTokenSecret = "";
+    }
+}
