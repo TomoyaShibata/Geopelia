ba4ede98e8c3df992f65b3333a6d7211abbd9df9
TomoyaShibata <wind.of.hometown@gmail.com>
2016-02-20 23:59:42 +0900

    [modify] 単体テスト実行のためにバグ回避の対応実施

--- Geopelia/App.xaml.cs
+++ Geopelia/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Data;
 using Geopelia.Models;
 using Prism.Unity.Windows;
 using Microsoft.Practices.Unity;
@@ -9,6 +10,7 @@ namespace Geopelia
     /// <summary>
     /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
     /// </summary>
+    [Bindable]
     sealed partial class App : PrismUnityApplication
     {
         /// <summary>
