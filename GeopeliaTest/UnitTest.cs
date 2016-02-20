2404e7cd37e7a16a2aed49fc6d00892568553cda
TomoyaShibata <wind.of.hometown@gmail.com>
2016-02-21 00:05:52 +0900

    [add] テストプロジェクトを追加

---
+++ GeopeliaTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace GeopeliaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var actual = 10 + 10;
+            Assert.AreEqual(20, actual);
+        }
+    }
+}
