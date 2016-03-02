1b7f514352177aa572d38e056e0cd1ed6a7ae3b9
TomoyaShibata <wind.of.hometown@gmail.com>
2016-03-02 21:42:09 +0900

    [clean] 消毒

--- Geopelia/Views/TweetCreatePage.xaml.cs
+++ Geopelia/Views/TweetCreatePage.xaml.cs
@@ -3,9 +3,6 @@ using Geopelia.ViewModels;
 
 namespace Geopelia.Views
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
     public sealed partial class TweetCreatePage : Page
     {
         private TweetCreatePageViewModel ViewModel => this.DataContext as TweetCreatePageViewModel;
