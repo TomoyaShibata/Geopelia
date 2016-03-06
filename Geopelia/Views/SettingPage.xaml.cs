1e18f22a588e873e9a814f677b436db8c7c89d37
TomoyaShibata <wind.of.hometown@gmail.com>
2016-03-06 20:58:37 +0900

    [wip] 設定画面実装中

---
+++ Geopelia/Views/SettingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+using Geopelia.ViewModels;
+
+namespace Geopelia.Views
+{
+    public sealed partial class SettingPage : Page
+    {
+        //private TweetCreatePageViewModel ViewModel => this.DataContext as TweetCreatePageViewModel;
+
+        public SettingPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
